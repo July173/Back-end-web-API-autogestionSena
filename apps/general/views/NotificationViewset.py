7630d69cd799307f40d4121953ab0503303f50d9
Reyving <medinafabriany@gmail.com>
2025-11-24 01:47:50 -0500

    feat: Update NotificationViewset to enhance Swagger documentation and modify URL path for notifications

--- apps/general/views/NotificationViewset.py
+++ apps/general/views/NotificationViewset.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 from apps.general.entity.models.Notification import Notification
 from apps.general.entity.serializers.NotificationSerializer import NotificationSerializer
 from apps.general.services.NotificationService import NotificationService
@@ -10,6 +12,10 @@ class NotificationViewset(viewsets.ModelViewSet):
     serializer_class = NotificationSerializer
     service_class = NotificationService
 
+    @swagger_auto_schema(
+        operation_description="Crea una nueva notificación.",
+        tags=["Notification"]
+    )
     @action(detail=False, methods=['post'], url_path='create')
     def create_notification(self, request):
         serializer = self.get_serializer(data=request.data)
@@ -17,12 +23,20 @@ class NotificationViewset(viewsets.ModelViewSet):
         notification = self.service_class().create_notification(serializer.validated_data)
         return Response(self.get_serializer(notification).data, status=status.HTTP_201_CREATED)
 
+    @swagger_auto_schema(
+        operation_description="Lista todas las notificaciones.",
+        tags=["Notification"]
+    )
     @action(detail=False, methods=['get'], url_path='list')
     def list_notifications(self, request):
         notifications = self.service_class().list_notifications()
         serializer = self.get_serializer(notifications, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+    @swagger_auto_schema(
+        operation_description="Obtiene una notificación por ID.",
+        tags=["Notification"]
+    )
     @action(detail=True, methods=['get'], url_path='get')
     def get_notification(self, request, pk=None):
         notification = self.service_class().get_notification(pk)
