133f0006c9e6fda7979d26dca00e76c8937621ac
july <danielitha345ra@gmail.com>
2025-11-26 12:36:10 -0500

    fix; Notification, Asgi, routine and setting; I fixed error to manage web sockets in front-end

--- core/asgi.py
+++ core/asgi.py
@@ -1,12 +1,17 @@
 
 import os
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
-import core.routing
+# Ensure DJANGO_SETTINGS_MODULE is set before importing any Django/ASGI modules
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter
+
 django_asgi_app = get_asgi_application()
+
+# Import routing after Django apps are initialized to avoid AppRegistryNotReady
+import core.routing
+
 application = ProtocolTypeRouter({
 	"http": django_asgi_app,
-	"websocket": core.routing.application["websocket"],
+	"websocket": core.routing.websocket_application,
 })
